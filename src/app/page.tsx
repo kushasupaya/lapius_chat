159e197b46ccb7a091d2351e77cc3656ac151660
kushasupaya <kushas.upaya@gmail.com>
2024-11-10 18:37:19 -0500

    entities fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [outputType, setOutputType] = useState<string>("text");
   const [medicalCodes, setMedicalCodes] = useState<ImageMedicalCode[]>([]);
 
@@ -38,10 +38,14 @@ export default function Home() {
     if (data.image === null) {
       setOutputType("text");
       await getClinicalNote(data, new AbortController().signal).then(
-        (response: ClinicalResponse) => {
+        (response: any) => {
           try {
-            setResponseData(response?.result);
-            console.log(response?.result);
+            const parsedResult = response.result.map((item: any) =>
+              JSON.parse(item)
+            );
+
+            setResponseData(parsedResult);
+            console.log(parsedResult);
             setIsLoading(false);
           } catch (e: any) {
             setIsLoading(false);
@@ -88,51 +92,81 @@ export default function Home() {
               />
               <button
                 type="submit"
-                className="w-1/4 mt-4 float-right py-2 bg-black text-white font-semibold rounded-md hover:bg-gray-800 transition"
+                className={`w-1/4 mt-4 float-right py-2 font-semibold rounded-md transition ${
+                  isLoading
+                    ? "bg-gray-400 text-gray-200 cursor-not-allowed"
+                    : "bg-black text-white hover:bg-gray-800"
+                }`}
+                disabled={isLoading}
               >
-                Analyze
+                {isLoading ? (
+                  <span className="flex space-x-1 ml-2">
+                    <span
+                      className="w-2 h-2 bg-gray-200 rounded-full animate-bounce"
+                      style={{ animationDelay: "0s" }}
+                    ></span>
+                    <span
+                      className="w-2 h-2 bg-gray-200 rounded-full animate-bounce"
+                      style={{ animationDelay: "0.1s" }}
+                    ></span>
+                    <span
+                      className="w-2 h-2 bg-gray-200 rounded-full animate-bounce"
+                      style={{ animationDelay: "0.2s" }}
+                    ></span>
+                  </span>
+                ) : (
+                  "Analyze"
+                )}
               </button>
             </form>
           </div>
-          <div className="border rounded-md bg-white w-[500px] h-[550px] overflow-y-auto p-6 ">
+          <div className="border rounded-md bg-white w-[500px] h-[550px] overflow-y-auto p-6">
             {isLoading ? (
               <LottieAnimation
                 src="/outputlot.json"
                 className="w-[400px]"
                 isAnimating={isLoading}
               />
+            ) : !responseData?.length && !medicalCodes?.length ? (
+              // Display message if there is no responseData and no medicalCodes
+              <div className="text-center text-gray-600">
+                Upload an image or enter your medical text
+              </div>
             ) : outputType === "text" ? (
               responseData?.map((item, index) => {
                 console.log("Full item object:", item);
-
-                console.log("List of entities:", item["list_of_entities"]);
+                const entities =
+                  item?.list_of_entities || item["list_of_entities"];
+                if (entities) {
+                  console.log("List of entities:", entities);
+                } else {
+                  console.log("List of entities is undefined or empty");
+                }
                 return (
-                  <MainDisclosure
-                    key={index}
-                    heading={
-                      item?.list_of_entities?.[0]?.icd10 || "No ICD-10 Code"
-                    }
-                    description={
-                      item?.list_of_entities?.[0]?.supported_evidence ||
-                      "No description available"
-                    }
-                  >
-                    <MiniDisclosure heading="Other codes">
-                      {item?.list_of_entities?.slice(1).map((entity, i) => (
-                        <div key={i} className="mt-2">
-                          <div className="font-semibold">{entity?.icd10}</div>
-                          <div className="text-gray-600">
-                            {entity?.supported_evidence}
+                  <div className="mt-2" key={index}>
+                    <MainDisclosure
+                      heading={entities?.[0]?.icd10 || "No ICD-10 Code"}
+                      description={
+                        entities?.[0]?.supported_evidence ||
+                        "No description available"
+                      }
+                    >
+                      <MiniDisclosure heading="Other codes">
+                        {entities?.slice(1).map((entity, i) => (
+                          <div key={i} className="mt-2">
+                            <div className="font-semibold">{entity?.icd10}</div>
+                            <div className="text-gray-600">
+                              {entity?.supported_evidence}
+                            </div>
                           </div>
-                        </div>
-                      ))}
-                    </MiniDisclosure>
-                  </MainDisclosure>
+                        ))}
+                      </MiniDisclosure>
+                    </MainDisclosure>
+                  </div>
                 );
               })
             ) : (
               <div>
-                {/* Conditionally render the heading if there are parsed image codes */}
                 {medicalCodes && medicalCodes.length > 0 && (
                   <h2 className="text-lg mb-4 text-left">
                     Your parsed image is below
